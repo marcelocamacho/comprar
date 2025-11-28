84e38d00f75925997d8195c56f6b33a6b3b4bc45
Marcelo Santana Camacho <marcelo.camacho@serpro.gov.br>
2025-11-28 13:37:30 -0300

    Componente filter com o statusIcon

--- src/app/components/Filter/index.tsx
+++ src/app/components/Filter/index.tsx
@@ -1,5 +1,6 @@
 import { FilterStatus } from "@/app/types/FilterStatus"
 import { StyleSheet, TouchableOpacity, TouchableOpacityProps, Text } from "react-native"
+import { StatusIcon } from "../StatusIcon"
 
 type Props = TouchableOpacityProps & {
     status: FilterStatus
@@ -13,7 +14,7 @@ export function Filter({ status, isActive, ...rest }: Props) {
             { opacity: isActive ? 1 : 0.5 }]}
             activeOpacity={0.8}
             {...rest} >
-
+            <StatusIcon status={status} />
             <Text style={styles.title}>
                 {status === FilterStatus.DONE ? "Comprados" : "Pendentes"}
             </Text>
@@ -22,7 +23,11 @@ export function Filter({ status, isActive, ...rest }: Props) {
 }
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 5
+    },
     title: {
         fontSize: 12,
         fontWeight: 600
--- src/app/components/StatusIcon/index.tsx
+++ src/app/components/StatusIcon/index.tsx
@@ -1,8 +1,9 @@
 import { FilterStatus } from "@/app/types/FilterStatus";
+import { CircleCheck, CircleDashed } from "lucide-react-native";
 
 
-export function StatusIcon({status}:{status:FilterStatus}){
-return(
-
-)
+export function StatusIcon({ status }: { status: FilterStatus }) {
+    return status === FilterStatus.DONE ? (
+        <CircleCheck size={18} color="#2C46B1" />
+    ) : (<CircleDashed size={18} color="#000000" />)
 }
